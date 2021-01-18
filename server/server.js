eeacb5333d15ff40973b538fe19e5d656afcf103
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-01-18 16:48:18 +0700

    'install-react-persiapan-mvc-server'

--- server/server.js
+++ server/server.js
@@ -2,15 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const URI_DB = process.env.URI_DB;
-const api = process.env.API
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT;
+const URI_DB = process.env.URI_DB;
+const api = process.env.API
 
 /**middleware */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
+app.use(morgan('tiny'));
+app.use(cors());
+app.options(`*`, cors());
 
 /**test route */
 app.get(`${api}`, (req, res) => {
