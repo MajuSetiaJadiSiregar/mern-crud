9700367f71aa9571e80a205a4752b09fe34a4e78
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-01-18 16:36:23 +0700

    'test-route-connect-database-done'

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-console.log('test nodemon');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const URI_DB = process.env.URI_DB;
+const api = process.env.API
+
+const app = express();
+const PORT = process.env.PORT;
+
+/**middleware */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+
+/**test route */
+app.get(`${api}`, (req, res) => {
+   res.json({ msg : 'router get berjalan'});
+});
+app.post(`/`, (req, res) => {
+   console.log(req.body);
+   res.json({ reqBody : 'berjalan'})
+});
+
+/**connect database */
+mongoose.connect(URI_DB, { useNewUrlParser : true, useUnifiedTopology : true})
+.then(() => { console.log(`database connect`)})
+.catch(err => { console.log(err)});
+
+
+/** http run */
+app.listen(PORT, () => { console.log(`server run di port ${PORT}`)});
