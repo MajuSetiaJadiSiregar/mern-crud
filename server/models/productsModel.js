48ab91ca71b7e0fc9a572c612056392e0eb1b50d
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-01-21 13:42:59 +0700

    'refactoring-code'

--- server/models/productsModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-   name : { type : String, required : true },
-   price : { type : Number, required : true }
-});
-
-module.exports = mongoose.model('Product', productSchema);
